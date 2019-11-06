2eaffc9be01b64c8a9ff885defd0b48e0d895c59
Andrew Bell <fiixed@gmail.com>
2019-11-06 13:18:35 -0600

    great placess provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/great_places.dart';
+import './screens/places_list_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
       ),
-      home: ,
     );
   }
 }
-
